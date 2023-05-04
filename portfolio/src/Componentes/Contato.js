58c70c29d217b6b6b214e611e3a82d98746cee62
gMarcondes00 <g.os.marcondes@gmail.com>
2023-05-04 09:47:29 -0300

    Sessao de contato feita

--- portfolio/src/Componentes/Contato.js
+++ portfolio/src/Componentes/Contato.js
@@ -1,7 +1,30 @@
 import './Assets/Contato.css';
+import 'font-awesome/css/font-awesome.min.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faWhatsapp } from '@fortawesome/free-brands-svg-icons'
 
+library.add();
 function Contato () {
-
+    return (
+        <div className="Contatos">
+            <h3>Contatos</h3>
+            <a href='mailto:g.os.marcondes@gmail.com' target='_blank' rel="noreferrer">
+                <FontAwesomeIcon icon={faEnvelope} className='iconeC'/>
+            </a>
+            <a href='https://github.com/gMarcondes00' target='_blank' rel="noreferrer">
+                <FontAwesomeIcon icon={faGithub} className='iconeC'/>
+            </a>
+            <a href='https://www.linkedin.com/in/gabriel-marcondes-690863256/' target='_blank' rel="noreferrer">
+                <FontAwesomeIcon icon={faLinkedin} className='iconeC'/>
+            </a>
+            <a href='https://api.whatsapp.com/send/?phone=5512996522060&text&type=phone_number&app_absent=0' target='_blank' rel="noreferrer">
+                <FontAwesomeIcon icon={faWhatsapp} className='iconeC'/>
+            </a>
+        
+        </div>
+    )
 }
 
 export default Contato
--- portfolio/src/Componentes/Projetos.js
+++ portfolio/src/Componentes/Projetos.js
@@ -12,32 +12,32 @@ function Projetos (){
             <div className="container">
                 <div className="wrapper">
                     <div>
-                        <a href="https://github.com/gMarcondes00/Rock-Paper-Scissor">
-                        <img src={PedraPapel} className="imagem" alt="" target="_blank"/>
+                        <a href="https://github.com/gMarcondes00/Rock-Paper-Scissor" target='_blank' rel="noreferrer">
+                        <img src={PedraPapel} className="imagem" alt=""/>
                         </a>
                         <p>Pedra Papel e Tesoura em JavaScript</p>
                     </div>
                     <div>
-                        <a href="https://github.com/gMarcondes00/CalculatorJs">
+                        <a href="https://github.com/gMarcondes00/CalculatorJs" target='_blank' rel="noreferrer">
                         <img src={Calculadora} className="imagem" alt="" target="_blank"/>
                         </a>
                         <p>Calculadora em JavaScript</p>
                     </div>
                     <div>
-                        <a href="https://github.com/gMarcondes00/Pomodoro">
-                        <img src={Pomodoro} className="imagem" alt="" target="_blank"/>
+                        <a href="https://github.com/gMarcondes00/Pomodoro" target='_blank' rel="noreferrer">
+                        <img src={Pomodoro} className="imagem" alt=""/>
                         </a>
                         <p>Pomodoro em JavaScript</p>
                     </div>
                     <div>
-                        <a href="https://github.com/gMarcondes00/quote-machine">
-                        <img src={Quote} className="imagem" alt="" target="_blank"/>
+                        <a href="https://github.com/gMarcondes00/quote-machine" target='_blank' rel="noreferrer">
+                        <img src={Quote} className="imagem" alt=""/>
                         </a>
                         <p>Gerador de citação em JavaScript com uso de API</p>
                     </div>
                     <div>
-                        <a href="https://github.com/gMarcondes00/TempoJS">
-                        <img src={Tempo} className="imagem" alt="" target="_blank"/>
+                        <a href="https://github.com/gMarcondes00/TempoJS" target='_blank' rel="noreferrer">
+                        <img src={Tempo} className="imagem" alt=""/>
                         </a>
                         <p>Previsão do tempo em JavaScript com uso de API</p>
                     </div>
